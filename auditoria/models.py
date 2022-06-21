f54c3a833d22f5d7f1edc572287f0bfa6bc08d44
matheus <matheus.mmn@hotmail.com>
2022-06-21 16:41:21 -0300

    'auditoria'

--- auditoria/models.py
+++ auditoria/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Auditoria(models.Model):
     objects = None
     tempo = models.CharField(max_length=5, blank=True)
+    nome = models.CharField(max_length=100, blank=True)
+    data = models.CharField(max_length=10, blank=True)
